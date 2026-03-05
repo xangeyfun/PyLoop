c394249069868efea988364d9bd666dd954bbdca
xangeyfun <kutkindje101@gmail.com>
2026-03-05 23:18:18 +0100

    Removed unnecessary file

--- restart.sh
+++
@@ -1,9 +0,0 @@
-echo "Restarting pyloop..."
-sudo systemctl restart pyloop
-echo ""
-echo "Restart done! Waiting 3 seconds..."
-sleep 3
-echo ""
-echo "Pyloop status: (press Q to exit)"
-echo ""
-sudo systemctl status pyloop
